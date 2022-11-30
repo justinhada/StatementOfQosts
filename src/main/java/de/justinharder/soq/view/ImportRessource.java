a6c804fcebc8b4e755b0096e516c2e2fa2b9bda0
Justin Harder <justinharder@t-online.de>
2022-11-30 18:24:30 +0100

    Starte mit Import-View

---
+++ src/main/java/de/justinharder/soq/view/ImportRessource.java
@@ -0,0 +1,34 @@
+package de.justinharder.soq.view;
+
+import de.justinharder.soq.domain.services.imports.ImportService;
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+import lombok.NonNull;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/import")
+public class ImportRessource
+{
+	@NonNull
+	private final ImportService importService;
+
+	@NonNull
+	private final Template imports;
+
+	public ImportRessource(@NonNull ImportService importService, @NonNull Template imports)
+	{
+		this.importService = importService;
+		this.imports = imports;
+	}
+
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public TemplateInstance zeigeImportFormular()
+	{
+		return imports.instance();
+	}
+}
