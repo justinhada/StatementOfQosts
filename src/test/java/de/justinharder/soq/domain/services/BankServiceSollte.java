6d3f74c7c5c2fa77fa779caac23be1c158a01f04
Justin Harder <justinharder@t-online.de>
2022-12-05 10:53:28 +0100

    Teste BankService

--- src/test/java/de/justinharder/soq/domain/services/BankServiceSollte.java
+++ src/test/java/de/justinharder/soq/domain/services/BankServiceSollte.java
@@ -1,7 +1,11 @@
 package de.justinharder.soq.domain.services;
 
 import de.justinharder.DtoTestdaten;
+import de.justinharder.soq.domain.model.Bank;
+import de.justinharder.soq.domain.model.meldung.Meldung;
+import de.justinharder.soq.domain.model.meldung.Schluessel;
 import de.justinharder.soq.domain.repository.BankRepository;
+import de.justinharder.soq.domain.services.dto.NeueBank;
 import de.justinharder.soq.domain.services.mapping.BankMapping;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,8 +16,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @DisplayName("BankService sollte")
 class BankServiceSollte extends DtoTestdaten
@@ -54,9 +57,65 @@ class BankServiceSollte extends DtoTestdaten
 	}
 
 	@Test
-	@DisplayName("erstellen")
+	@DisplayName("leere Eingabedaten melden")
 	void test03()
 	{
-		// TODO: Testen
+		var ergebnis = sut.erstelle(new NeueBank(LEER, LEER));
+
+		assertAll(
+			() -> assertThat(ergebnis.getMeldungen(Schluessel.BEZEICHNUNG)).containsExactlyInAnyOrder(
+				Meldung.BEZEICHNUNG_LEER),
+			() -> assertThat(ergebnis.getMeldungen(Schluessel.BIC)).containsExactlyInAnyOrder(Meldung.BIC_LEER),
+			() -> assertThat(ergebnis.getMeldungen(Schluessel.ALLGEMEIN)).isEmpty());
+	}
+
+	@Test
+	@DisplayName("ungültige BIC melden")
+	void test04()
+	{
+		var ergebnis = sut.erstelle(new NeueBank(BEZEICHNUNG_1_WERT, "OLBODEH2XXXX"));
+
+		assertAll(
+			() -> assertThat(ergebnis.getMeldungen(Schluessel.BEZEICHNUNG)).isEmpty(),
+			() -> assertThat(ergebnis.getMeldungen(Schluessel.BIC)).containsExactlyInAnyOrder(Meldung.BIC_UNGUELTIG),
+			() -> assertThat(ergebnis.getMeldungen(Schluessel.ALLGEMEIN)).isEmpty());
+	}
+
+	@Test
+	@DisplayName("bereits existierende Daten melden")
+	void test05()
+	{
+		when(bankRepository.istVorhanden(BEZEICHNUNG_1)).thenReturn(true);
+		when(bankRepository.istVorhanden(BIC_1)).thenReturn(true);
+
+		var ergebnis = sut.erstelle(new NeueBank(BEZEICHNUNG_1_WERT, BIC_1_WERT));
+
+		assertAll(
+			() -> assertThat(ergebnis.getMeldungen(Schluessel.BEZEICHNUNG)).containsExactlyInAnyOrder(
+				Meldung.BEZEICHNUNG_EXISTIERT_BEREITS),
+			() -> assertThat(ergebnis.getMeldungen(Schluessel.BIC)).containsExactlyInAnyOrder(
+				Meldung.BIC_EXISTIERT_BEREITS),
+			() -> assertThat(ergebnis.getMeldungen(Schluessel.ALLGEMEIN)).isEmpty());
+		verify(bankRepository).istVorhanden(BEZEICHNUNG_1);
+		verify(bankRepository).istVorhanden(BIC_1);
+	}
+
+	@Test
+	@DisplayName("erstellen")
+	void test06()
+	{
+		when(bankRepository.istVorhanden(BEZEICHNUNG_1)).thenReturn(false);
+		when(bankRepository.istVorhanden(BIC_1)).thenReturn(false);
+
+		var ergebnis = sut.erstelle(new NeueBank(BEZEICHNUNG_1_WERT, BIC_1_WERT));
+
+		assertAll(
+			() -> assertThat(ergebnis.getMeldungen(Schluessel.BEZEICHNUNG)).isEmpty(),
+			() -> assertThat(ergebnis.getMeldungen(Schluessel.BIC)).isEmpty(),
+			() -> assertThat(ergebnis.getMeldungen(Schluessel.ALLGEMEIN)).containsExactlyInAnyOrder(
+				Meldung.BANK_ERSTELLT));
+		verify(bankRepository).istVorhanden(BEZEICHNUNG_1);
+		verify(bankRepository).istVorhanden(BIC_1);
+		verify(bankRepository).speichere(any(Bank.class));
 	}
 }
