141375b677ede6cbbdde13bf4df2dc395751a61e
Justin Harder <justinharder@t-online.de>
2022-12-12 08:45:34 +0100

    Stelle auf RequestScoped um

--- src/main/java/de/justinharder/soq/view/StartRessource.java
+++ src/main/java/de/justinharder/soq/view/StartRessource.java
@@ -2,12 +2,14 @@ package de.justinharder.soq.view;
 
 import io.quarkus.qute.TemplateInstance;
 
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+@RequestScoped
 @Path("/start")
 public class StartRessource
 {
