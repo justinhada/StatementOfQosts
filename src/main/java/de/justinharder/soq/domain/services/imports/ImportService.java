5c923b725bad412bb8ac242f53a59508dc5b2814
Justin Harder <justinharder@t-online.de>
2022-12-31 12:49:13 +0100

    Beginne mit TODOs fuer ImportService

--- src/main/java/de/justinharder/soq/domain/services/imports/ImportService.java
+++ src/main/java/de/justinharder/soq/domain/services/imports/ImportService.java
@@ -1,15 +1,45 @@
 package de.justinharder.soq.domain.services.imports;
 
+import de.justinharder.soq.domain.model.attribute.Datei;
+import de.justinharder.soq.domain.model.attribute.Herausgeber;
+import de.justinharder.soq.domain.repository.UmsatzRepository;
 import de.justinharder.soq.domain.services.dto.NeuerImport;
+import de.justinharder.soq.domain.services.imports.model.Import;
+import io.vavr.control.Validation;
 import lombok.NonNull;
 
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 
 @Dependent
 public class ImportService
 {
+	@NonNull
+	private final UmsatzRepository umsatzRepository;
+
+	@Inject
+	public ImportService(@NonNull UmsatzRepository umsatzRepository)
+	{
+		this.umsatzRepository = umsatzRepository;
+	}
+
 	public NeuerImport importiere(@NonNull NeuerImport neuerImport)
 	{
-		return null;
+		/*
+			1 Herausgeber validieren.
+			2 Datei validieren.
+			3 Import aus Herausgeber & Datei erstellen.
+			4 CSV aus Datei erstellen.
+			5 UmsatzDaten aus CSV - abhängig vom Herausgeber - erstellen.
+			  a OLB-/VRB-UmsatzDatum aus Zeile erstellen.
+			  b UmsatzDatum aus OLB-/VRB-UmsatzDatum erstellen.
+			  c UmsatzDaten aus allen UmsatzDatum erstellen.
+			6 ...
+		 */
+
+		Validation.combine(Herausgeber.aus(neuerImport.getHerausgeber()), Datei.aus(neuerImport.getDatei()))
+			.ap(Import::aus);
+
+		return neuerImport;
 	}
 }
--- src/test/java/de/justinharder/soq/domain/services/imports/ImportServiceSollte.java
+++ src/test/java/de/justinharder/soq/domain/services/imports/ImportServiceSollte.java
@@ -1,25 +1,33 @@
 package de.justinharder.soq.domain.services.imports;
 
 import de.justinharder.Testdaten;
+import de.justinharder.soq.domain.repository.UmsatzRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
 
 @DisplayName("ImportService sollte")
 class ImportServiceSollte extends Testdaten
 {
 	private ImportService sut;
 
+	private UmsatzRepository umsatzRepository;
+
 	@BeforeEach
 	void setup()
 	{
-		sut = new ImportService();
+		umsatzRepository = mock(UmsatzRepository.class);
+
+		sut = new ImportService(umsatzRepository);
 	}
 
 	@Test
 	@DisplayName("null validieren")
 	void test01()
 	{
-
+		assertThrows(NullPointerException.class, () -> new ImportService(null));
 	}
 }
