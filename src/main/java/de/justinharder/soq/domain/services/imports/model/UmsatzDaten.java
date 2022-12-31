aa1c51800b505c5581604e2cfedafeecaaca5ec5
Justin Harder <justinharder@t-online.de>
2022-12-31 12:48:12 +0100

    Sammle alle UmsatzDatum zu UmsatzDaten

---
+++ src/main/java/de/justinharder/soq/domain/services/imports/model/UmsatzDaten.java
@@ -0,0 +1,24 @@
+package de.justinharder.soq.domain.services.imports.model;
+
+import de.justinharder.soq.domain.model.meldung.Meldung;
+import de.justinharder.soq.domain.model.meldung.Meldungen;
+import io.vavr.control.Option;
+import io.vavr.control.Validation;
+
+import java.io.Serial;
+import java.util.ArrayList;
+
+public class UmsatzDaten extends ArrayList<UmsatzDatum>
+{
+	@Serial
+	private static final long serialVersionUID = 9132312866401384922L;
+
+	public static Validation<Meldungen, UmsatzDaten> aus(Import importObjekt)
+	{
+		Option.of(importObjekt)
+			.toValidation(Meldungen.aus(Meldung.IMPORT))
+			.map(Import::getDatei);
+
+		return null;
+	}
+}
