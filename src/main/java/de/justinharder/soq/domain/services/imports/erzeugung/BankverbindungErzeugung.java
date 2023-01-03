6aa31c3a5bbad9e5808e9395a38bd94e386bc089
Justin Harder <justinharder@t-online.de>
2023-01-03 23:55:06 +0100

    Finde oder erzeuge Bankverbindung aus UmsatzDatum

---
+++ src/main/java/de/justinharder/soq/domain/services/imports/erzeugung/BankverbindungErzeugung.java
@@ -0,0 +1,54 @@
+package de.justinharder.soq.domain.services.imports.erzeugung;
+
+import de.justinharder.soq.domain.model.Bankverbindung;
+import de.justinharder.soq.domain.model.attribute.BIC;
+import de.justinharder.soq.domain.model.attribute.IBAN;
+import de.justinharder.soq.domain.model.meldung.Meldung;
+import de.justinharder.soq.domain.model.meldung.Meldungen;
+import de.justinharder.soq.domain.repository.BankverbindungRepository;
+import io.vavr.control.Validation;
+import lombok.NonNull;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+import java.util.function.Function;
+
+@Dependent
+public class BankverbindungErzeugung
+{
+	@NonNull
+	private final BankverbindungRepository bankverbindungRepository;
+
+	@NonNull
+	private final BankErzeugung bankErzeugung;
+
+	@Inject
+	public BankverbindungErzeugung(
+		@NonNull BankverbindungRepository bankverbindungRepository,
+		@NonNull BankErzeugung bankErzeugung)
+	{
+		this.bankverbindungRepository = bankverbindungRepository;
+		this.bankErzeugung = bankErzeugung;
+	}
+
+	public Validation<Meldungen, Bankverbindung> findeAuftraggeber(@NonNull IBAN iban)
+	{
+		return bankverbindungRepository.finde(iban).toValidation(Meldungen.aus(Meldung.BANKVERBINDUNG_EXISTIERT_NICHT));
+	}
+
+	// TODO: Funktionaler gestalten
+	public Validation<Meldungen, Bankverbindung> findeOderErzeuge(@NonNull IBAN iban, @NonNull BIC bic)
+	{
+		var bankverbindung = bankverbindungRepository.finde(iban);
+		if (bankverbindung.isDefined())
+		{
+			return bankverbindung.toValidation(Meldungen.aus(Meldung.BANKVERBINDUNG_EXISTIERT_NICHT));
+		}
+		return Validation.combine(
+				Validation.valid(iban),
+				bankErzeugung.findeOderErzeuge(bic))
+			.ap(Bankverbindung::aus)
+			.mapError(Meldungen::aus)
+			.flatMap(Function.identity());
+	}
+}
---
+++ src/test/java/de/justinharder/soq/domain/services/imports/erzeugung/BankverbindungErzeugungSollte.java
@@ -0,0 +1,76 @@
+package de.justinharder.soq.domain.services.imports.erzeugung;
+
+import de.justinharder.ImportTestdaten;
+import de.justinharder.soq.domain.repository.BankverbindungRepository;
+import io.vavr.control.Option;
+import io.vavr.control.Validation;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@DisplayName("BankverbindungErzeugung sollte")
+class BankverbindungErzeugungSollte extends ImportTestdaten
+{
+	private static final String NAME = "Laura Tiemerding";
+
+	private BankverbindungErzeugung sut;
+
+	private BankverbindungRepository bankverbindungRepository;
+	private BankErzeugung bankErzeugung;
+
+	@BeforeEach
+	void setup()
+	{
+		bankverbindungRepository = mock(BankverbindungRepository.class);
+		bankErzeugung = mock(BankErzeugung.class);
+
+		sut = new BankverbindungErzeugung(bankverbindungRepository, bankErzeugung);
+	}
+
+	@Test
+	@DisplayName("null validieren")
+	void test01()
+	{
+		assertAll(
+			() -> assertThrows(NullPointerException.class, () -> new BankverbindungErzeugung(null, bankErzeugung)),
+			() -> assertThrows(NullPointerException.class,
+				() -> new BankverbindungErzeugung(bankverbindungRepository, null)),
+			() -> assertThrows(NullPointerException.class, () -> sut.findeOderErzeuge(null, BIC_1)),
+			() -> assertThrows(NullPointerException.class, () -> sut.findeOderErzeuge(IBAN_1, null)));
+	}
+
+	@Test
+	@DisplayName("Bank finden und nicht neu erzeugen")
+	void test02()
+	{
+		when(bankverbindungRepository.finde(IBAN_1)).thenReturn(Option.of(BANKVERBINDUNG_1));
+
+		var ergebnis = sut.findeOderErzeuge(IBAN_1, BIC_1);
+		assertAll(
+			() -> assertThrows(RuntimeException.class, ergebnis::getError),
+			() -> assertThat(ergebnis.get()).isEqualTo(BANKVERBINDUNG_1));
+		verify(bankverbindungRepository).finde(IBAN_1);
+	}
+
+	@Test
+	@DisplayName("Bank nicht finden und neu erzeugen")
+	void test03()
+	{
+		when(bankverbindungRepository.finde(IBAN_1)).thenReturn(Option.none());
+		when(bankErzeugung.findeOderErzeuge(BIC_1)).thenReturn(Validation.valid(BANK_1));
+
+		var validierung = sut.findeOderErzeuge(IBAN_1, BIC_1);
+		var ergebnis = validierung.get();
+		assertAll(
+			() -> assertThrows(RuntimeException.class, validierung::getError),
+			() -> assertThat(ergebnis.getIban()).isEqualTo(IBAN_1),
+			() -> assertThat(ergebnis.getBank()).isEqualTo(BANK_1));
+		verify(bankverbindungRepository).finde(IBAN_1);
+		verify(bankErzeugung).findeOderErzeuge(BIC_1);
+	}
+}
