f7bc7c86b4e8963f7b67f0c92b7d0247448dc4d7
Justin Harder <justinharder@t-online.de>
2023-08-03 17:26:13 +0200

    Erstelle ThemeRessource

---
+++ src/main/java/de/justinharder/soq/view/ThemeRessource.java
@@ -0,0 +1,27 @@
+package de.justinharder.soq.view;
+
+import lombok.Getter;
+
+import javax.enterprise.context.SessionScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.io.Serializable;
+
+@Getter
+@SessionScoped
+@Path("/theme")
+public class ThemeRessource implements Serializable
+{
+	private String theme = "dark";
+
+	@POST
+	@Consumes
+	@Produces(MediaType.TEXT_PLAIN)
+	public void wechsleTheme()
+	{
+		theme = theme.equals("dark") ? "light" : "dark";
+	}
+}
